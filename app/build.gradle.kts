3827817069ad7e7d48d468127dace149e7aa172f
osabra <arezvo92@gmail.com>
2026-08-28 03:08:09 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
     namespace = "com.osabra.superaijarvis"
     compileSdk = 34
@@ -28,12 +29,15 @@ android {
     }
     composeOptions { kotlinCompilerExtensionVersion = "1.5.8" }
 }
+
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
+    val composeBom = platform("androidx.compose:compose-bom:2024.02.00")
+    implementation(composeBom)
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.compose.ui:ui:1.5.8")
-    implementation("androidx.compose.material3:material3:1.1.2")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.8")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("com.google.ai.client.generativeai:generativeai:0.3.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
