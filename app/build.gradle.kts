437757da8853158a4a1e6fcf622bf6ab15161b25
osabra <arezvo92@gmail.com>
2026-08-30 08:47:58 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.11"
+        kotlinCompilerExtensionVersion = "1.5.8"
     }
     packaging {
         resources {
